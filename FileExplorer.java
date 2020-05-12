894a0f1b05d2d72aeb916b19d111bddae8c6cbe9
Matt <mdjacks@bgsu.edu>
2020-05-12 10:16:56 -0400

    Finished Daily Java Project for 5/12

---
+++ FileExplorer.java
@@ -0,0 +1,145 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+/*
+ * FileExplorer
+ *
+ * Description: Simple implementation of a file explorer.
+ */
+
+class FileExplorer {
+
+    File file;
+
+    /*
+     * FileExplorer()
+     *
+     * Description: Constructor that doesn't accept any inputs
+     *
+     * Arguments:
+     *  N/A
+     *
+     * Returns:
+     *  N/A
+     */
+    FileExplorer() {  }
+
+    /*
+     * setFile()
+     *
+     * Description: Setter function for this.file
+     *
+     * Arguments:
+     *  String - address - a string version of the address of the file or 
+     *      directory to be read
+     *
+     * Returns:
+     *  N/A
+     */
+    public void setFile(String address) throws NullPointerException, IllegalAccessException {
+        if (new File(address).exists()) {
+            this.file = new File(address);
+        } else {
+            throw new IllegalArgumentException("File/Folder "+address+" does not exist");
+        }
+    }
+
+    /*
+     * getFile()
+     *
+     * Description: Getter function for this.file
+     *
+     * Arguments:
+     *  N/A
+     *
+     * Returns:
+     *  File - the file used by this FileExplorer
+     */
+    public File getFile() {
+        return this.file;
+    }
+
+    /*
+     * getContainedFiles()
+     *
+     * Description: Returns the names of the files contained in the given
+     *      directory in a list. Use getFile().list() for an array of 
+     *      file & subdirectory names
+     *
+     * Arguments:
+     *  N/A
+     *
+     * Returns:
+     *  String - Prettied list of the subfiles and subdirectories
+     */
+    public String getContainedFiles() {
+        String contents = "";
+
+        if(this.getFile().isDirectory()) {
+
+            for (String file: this.getFile().list()) {
+                contents += "\n- "+file;
+            }
+
+            return contents;
+
+        } else {
+            throw new IllegalArgumentException(this.getFile().toString()+" is not a directory");
+        }    
+    }
+
+    /*
+     * getFileContents()
+     *
+     * Description: Returns the contents of the given file, line by line
+     *
+     * Arguments:
+     *  N/A
+     *
+     * Returns:
+     *  String - contents of the given file
+     */
+    public String getFileContents() throws IllegalArgumentException, FileNotFoundException {
+        String contents = "";
+        if (this.getFile().canRead()) {
+            Scanner scanner = new Scanner(this.getFile());
+
+            while(scanner.hasNextLine()) {
+                contents += scanner.nextLine() + "\n";
+            }
+        } else {
+            throw new IllegalArgumentException("Unable to read file contents of "+this.getFile().toString());
+        }
+
+        return contents;   
+    }
+
+    /*
+     * agnosticGetContents()
+     *
+     * Description: Runs either .getFileContents() or .getContainedFiles()
+     *      based on whether this.file is a directory or file
+     *
+     * Arguments:
+     *  N/A
+     *
+     * Returns:
+     *  String - Prettied contents of the file or directory
+     */
+    public String agnosticGetContents() throws IllegalArgumentException, FileNotFoundException, IllegalAccessException {
+        if (this.getFile().isDirectory()) {
+
+            return this.getContainedFiles();
+
+        } else if (this.getFile().isFile()) {
+
+            return this.getFileContents();
+
+        } else {
+
+            throw new IllegalAccessException(this.getFile().toString()+" is neither a file nor a directory");
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,52 @@
+# FileExplorer
+## Daily Java Project for: 5/11/2020
+
+### Description:
+Simple implementation of a file explorer. Can display the files and folders within a directory or the contents of a readable file.
+
+### Daily Java Projects
+I'm creating a small Java project each day to help me learn Java. These aren't rigorously tested, but instead exist just so I can get a better understanding of some portion of Java.
+
+### Points of Interest
+
++ I think I've been overusing methods that print things, instead I should just return the string and let the user print it if they want.
++ Got better at throwing exceptions for errors
++ Noticed you seem to need a constructor even if it's not accepting arguments or doing anything 
+
+### Using `FileExplorer`
+
++ Example: Showing the contents of this directory and displaying the contents of `README.md`
+
+```
+public class Parent {
+    public static void main(String args[]) {
+        FileExplorer fileExp = new FileExplorer();
+        try {
+            fileExp.setFile(".");
+            System.out.println(fileExp.getContainedFiles());
+
+            fileExp.setFile("./README.md");
+            System.out.println(fileExp.getFileContents());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+```
+
+### Documentation
+
++ `FileExplorer()`
+    + Constructor that doesn't accept any inputs
++ `public void setFile(String address) throws NullPointerException, IllegalAccessException`
+    + Sets the address of the file or directory to be read
++ ` public File getFile()`
+    + Returns the File object being used
++ `public String getContainedFiles()`
+    + Returns the names of the files contained in the given directory in a list.
++ `public String getFileContents() throws IllegalArgumentException, FileNotFoundException`
+    + Returns the contents of the given file, line by line
++ `agnosticGetContents()`
+    + Runs either .getFileContents() or .getContainedFiles() based on whether .getFile() returns a directory or file
